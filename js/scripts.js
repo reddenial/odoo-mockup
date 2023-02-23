5a9601df2f7c460580a29a70a5fb58d1e329e932
reddenial <danielgdrojas@gmail.com>
2023-02-23 14:52:27 +0100

    sticky header added

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
+//sticky navigation
+window.onscroll = function () { stickyNav() };
+var lastScrollTop = 0;
 
+function stickyNav() {
+    const doc = document.documentElement;
+    let scrollTop = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+    const header = document.getElementsByTagName('header')[0];
+    const headerH = header.getBoundingClientRect().height;
 
-console.log('hello');
+    console.log(scrollTop);
 
+    if (scrollTop > headerH) {
+        header.classList.add('fixed');
+    } else {
+        header.classList.remove('fixed');
+    } 
+
+}
+
+//init swiper
 const swiper = new Swiper(".swiper", {
     slidesPerView: 5,
     centeredSlides: true,
